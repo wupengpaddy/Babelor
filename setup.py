b648c5084a58b6e5262a10b5f95d138878847496
StrTrek <30856158+strtrek@users.noreply.github.com>
2019-04-24 21:22:35 +0800

    优化文件注释

--- setup.py
+++ setup.py
@@ -10,13 +10,16 @@ except ImportError:
 
 def load_rst_file(rst_file_path):
     """
-    load long description from RST file，publish on PyPI
-    载入长描述文件，发布在 PyPI 的项目库信息页面上
+    load long description from RST file，publish on PyPi
+    从 RST 文件载入长描述，发布在 PyPi 的项目库信息页面上
     """
     return codecs.open(os.path.join(os.path.dirname(__file__), rst_file_path)).read()
 
 
-NAME = "Babelor"                        # package name          库名
+# 库名
+NAME = "Babelor"
+
+# 库包含路径
 PACKAGES = ["Babelor",                  # root/ path            库/根
             "Babelor/Config",           # root/config path      库/配置层
             "Babelor/DataBase",         # root/database path    库/数据库层
@@ -26,6 +29,8 @@ PACKAGES = ["Babelor",                  # root/ path            库/根
             "Babelor/Session",          # root/session          库/会话层
             "Babelor/Tools",            # root/tools            库/工具层
             ]
+
+# 库依赖外部包
 INSTALL_REQUIRED = [
     "requests>=2.18.4",         # python http connector
     "beautifulsoup4>=4.6.0",    # python html4 descriptor
@@ -36,6 +41,39 @@ INSTALL_REQUIRED = [
     "pyzmq>=17.1.2",            # python binding for 0mq
     "pyftpdlib>=1.5.4",         # python async ftp server
 ]
+
+# 库分类
+CLASSIFIERS = [
+    # 开发状态
+    #  1 - Planning             2 - Pre-Alpha       3 - Alpha       4 - Beta
+    #  5 - Production/Stable    6 - Mature          7 - Inactive
+    'Development Status :: 2 - Pre-Alpha',
+    # 许可证
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    'License :: OSI Approved :: Apache Software License',
+    # 目标用户群体
+    'Intended Audience :: Developers',
+    'Intended Audience :: Financial and Insurance Industry',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Manufacturing',
+    'Intended Audience :: Healthcare Industry',
+    # 主题
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: System :: Distributed Computing',
+    # 目标 Python 版本
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    # 目标操作系统
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+]
+
+# 简单描述
 DESCRIPTION = """
 Babelor - a ting data analysis and manipulation library for Python
 =====================================================================
@@ -48,17 +86,31 @@ the broader goal of becoming **the most powerful and flexible open source data
 analysis / manipulation tool available in any language**. It is already well on
 its way toward this goal.
 """
+
+# 详细描述
 if os.path.exists("README.rst"):
     LONG_DESCRIPTION = load_rst_file("README.rst")
 else:
     LONG_DESCRIPTION = open("README.txt", "r").read()
-KEYWORDS = "Service Bus "
-AUTHOR = "StrTrek Team Authors"
-AUTHOR_EMAIL = "geyuanji@strtrek.com"
-MAINTAINER = "StrTrek Team Authors"
-MAINTAINER_EMAIL = "geyuanji@strtrek.com"
-URL = "http://www.strtrek.com/"
+
+# 关键字
+KEYWORDS = "Service Bus"
+AUTHOR = "StrTrek Team Authors"             # 作者
+AUTHOR_EMAIL = "geyuanji@strtrek.com"       # 作者联系邮件
+MAINTAINER = "StrTrek Team Authors"         # 维护者
+MAINTAINER_EMAIL = "geyuanji@strtrek.com"   # 维护者联系邮件
+URL = "http://www.strtrek.com/"             # 项目主页
+# 版本号
+# r"N.N[.N]+[{a|b|c|rc}N[.N]+][.postN][.devN]"
+# N.N           主版本号和副版本号
+# [.N]          次要版本号
+# {a|b|c|rc}    阶段代号(可选), a:alpha, b:beta, c:candidate, rc:release candidate
+# N[.N]         阶段主版本号和副版本号(可选)
+# .postN        发行后的更新版本号(可选)
+# .devN         开发期间的发行版本号(可选)
 VERSION = "0.1.1"
+
+# 许可证
 LICENSE = """
 Copyright 2019 StrTrek Team Authors.
 
@@ -74,30 +126,22 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-setup(name=NAME, version=VERSION, description=DESCRIPTION, long_description=LONG_DESCRIPTION,
-      classifiers=[
-          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-          'License :: OSI Approved :: Apache Software License',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Intended Audience :: StrTrek Team Authors',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX',
-      ], keywords=KEYWORDS, author=AUTHOR, author_email=AUTHOR_EMAIL, url=URL, license=LICENSE,
-      packages=PACKAGES, include_package_data=True, zip_safe=True, install_requires=INSTALL_REQUIRED,)
 
-# check distribute code 校验检查
+# 封库
+setup(name=NAME, version=VERSION, description=DESCRIPTION, long_description=LONG_DESCRIPTION, classifiers=CLASSIFIERS,
+      keywords=KEYWORDS, author=AUTHOR, author_email=AUTHOR_EMAIL, url=URL, license=LICENSE, packages=PACKAGES,
+      include_package_data=True, zip_safe=True, install_requires=INSTALL_REQUIRED,)
+
+# 外部执行封库命令
+
+# 校验检查
 # python setup.py check
 
-# package all               封库
-# .zip or .tar.gz in dist/  打包文件为zip或tar.gz
+# 封库到 .zip or .tar.gz in dist/
 # python setup.py sdist
 
-# build wheel file      编译本地安装包
-# whl file in dist/     安装包位于 dist/ 路径下
+# 编译为 WHL in dist/
 # python setup.py bdist_wheel
 
-# register and upload to PyPI   编译上传至 PyPI
+# 编译上传至 PyPi
 # python setup.py register sdist upload
