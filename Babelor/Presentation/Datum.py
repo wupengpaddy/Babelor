e607281585fcbb125d928360c59129dad27ccc02
StrTrek <30856158+strtrek@users.noreply.github.com>
2019-05-12 11:24:21 +0800

    let datum support numpy

--- Babelor/Presentation/Datum.py
+++ Babelor/Presentation/Datum.py
@@ -21,6 +21,7 @@ import pandas as pd
 import numpy as np
 # Inner Required
 from Babelor.Presentation.UniformResourceIdentifier import URL, url_null_keep
+from Babelor.Tools import dict2json, json2dict
 # Global Parameters
 from Babelor.Config import CONFIG
 
@@ -31,7 +32,7 @@ class DATUM:
         self.stream = None
         self.coding = None
         self.path = None
-        self.dtype = None
+        self.type = None
 
     def add(self, datum, path: (str, URL) = None):
         dt = datum_to_stream(datum)
@@ -40,12 +41,12 @@ class DATUM:
             self.stream = [dt["stream"], ]
             self.coding = [dt["coding"], ]
             self.path = [path, ]
-            self.dtype = [dt["dtype"], ]
+            self.type = [dt["type"], ]
         else:
             self.stream = self.stream + [dt["stream"], ]
             self.coding = self.coding + [dt["coding"], ]
             self.path = self.path + [path, ]
-            self.dtype = self.dtype + [dt["dtype"], ]
+            self.type = self.type + [dt["type"], ]
         self.count = self.count + 1
 
     def remove(self, idx: int):
@@ -59,11 +60,18 @@ class DATUM:
             del self.__dict__["stream"][idx]
             del self.__dict__["coding"][idx]
             del self.__dict__["path"][idx]
-            del self.__dict__["dtype"][idx]
+            del self.__dict__["type"][idx]
             self.count = self.__dict__["count"] - 1
     
     def read(self, idx: int):
-
+        if self.count == 0:
+            return {"stream": None, "path": None}
+        else:
+            return {
+                "stream": stream_to_datum(self.__dict__["stream"][idx - 1], self.__dict__["coding"][idx - 1],
+                                          self.__dict__["dtype"][idx - 1]),
+                "path": self.__dict__["path"][idx - 1],
+            }
 
 
 def datum_to_stream(datum: (str, pd.DataFrame, bytes, bytearray, None)) -> dict:
@@ -73,44 +81,47 @@ def datum_to_stream(datum: (str, pd.DataFrame, bytes, bytearray, None)) -> dict:
     {
         "stream": [None, str],
         "coding": [None, "ascii"],
-        "dtype": [None, "str", "base64", "pandas.core.frame.DataFrame"]
+        "type": [None, "str", "base64", "pandas.core.frame.DataFrame"]
     }
     """
     if datum is None:
         rt = {
             "stream": None,
             "coding": None,
-            "dtype": None,
+            "type": None,
         }
     elif isinstance(datum, str):
         rt = {
             "stream": base64.b64encode(datum.encode(CONFIG.Coding)).decode("ascii"),
             "coding": CONFIG.Coding,
-            "dtype": "str",
+            "type": "str",
         }
     elif isinstance(datum, pd.DataFrame):
         rt = {
             "stream": base64.b64encode(datum.to_msgpack()).decode("ascii"),
             "coding": None,
-            "dtype": "pandas.core.frame.DataFrame",
+            "type": "pandas.core.frame.DataFrame",
         }
     elif isinstance(datum, np.ndarray):
         rt = {
             "stream": base64.b64encode(datum).decode("ascii"),
-            "coding": None,
-            "dtype": "numpy.ndarray",
+            "coding": dict2json({
+                "dtype": str(datum.dtype),
+                "shape": datum.shape,
+             }),
+            "type": "numpy.ndarray",
         }
     elif isinstance(datum, (bytes, bytearray)):
         rt = {
             "stream": base64.b64encode(datum).decode("ascii"),
             "coding": "ascii",
-            "dtype": "base64",
+            "type": "base64",
         }
     else:
         rt = {
             "stream": None,
             "coding": None,
-            "dtype": None,
+            "type": None,
         }
     return rt
 
@@ -128,5 +139,9 @@ def stream_to_datum(stream: str = None, coding: str = None, dtype: str = None):
         return base64.b64decode(stream.encode("ascii")).decode(coding)
     elif dtype in ["pandas.core.frame.DataFrame"]:
         return pd.read_msgpack(base64.b64decode(stream.encode(coding)))
+    elif dtype in ["numpy.ndarray"]:
+        code = json2dict(coding)
+        rt = np.frombuffer(base64.decodebytes(stream.encode("ascii")), dtype=np.dtype(code["dtype"]))
+        return np.reshape(rt, code["shape"])
     else:
         return None
