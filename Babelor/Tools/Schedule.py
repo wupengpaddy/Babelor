dc4b8f829661655a9a64d15d141716e0fb3e4f23
strtrek <geyuanji@strtrek.com>
2019-05-15 15:51:03 +0800

    fix TASKS can't afford args

--- Babelor/Tools/Schedule.py
+++ Babelor/Tools/Schedule.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # System Required
-import datetime
+from datetime import datetime, timedelta
 import time
 # Outer Required
 # import msgpack
@@ -24,61 +24,64 @@ from Babelor.Config import CONFIG
 
 
 class TASKS:
-    def __init__(self):
+    def __init__(self, interval: int = CONFIG.TASK_BLOCK_TIME):
         self.tasks = []
+        self.interval = interval
         self.active = True
 
     def run_tasks(self):
-        runnable_tasks = [task for task in self.tasks if task.should_run]
-        if len(runnable_tasks) < 1:
-            self.active = False
+        runnable_tasks = [task for task in self.tasks if task.should_run()]
         for task in sorted(runnable_tasks):
             task.run()
+        # --------------------
+        next_run_tasks_cnt = 0
+        for task in self.tasks:
+            if task.next_should_run():
+                next_run_tasks_cnt += 1
+        if next_run_tasks_cnt == 0:
+            self.active = False
 
-    def remove(self, task):
-        try:
-            self.tasks.remove(task)
-        except ValueError:
-            pass
-
-    def add(self, start_datetime: datetime.datetime, timedelta: datetime.timedelta,
-            func: callable, expire_run_times: int = CONFIG.TASK_MAX_RUN_TIMES):
-        task = TASK(start_datetime, timedelta, expire_run_times, self, func)
+    def add(self, start: datetime, delta: timedelta, func: callable,
+            expired: int = CONFIG.TASK_MAX_RUN_TIMES, **kwargs):
+        task = TASK(start, delta, expired, func, **kwargs)
         self.tasks.append(task)
 
     def start(self):
         while self.active:
             self.run_tasks()
-            time.sleep(CONFIG.TASK_BLOCK_TIME)
+            time.sleep(self.interval)
         else:
             self.active = True
 
 
 class TASK:
-    def __init__(self, start_datetime: datetime.datetime, timedelta: datetime.timedelta,
-                 expire_run_times: int, tasks: TASKS, func: callable):
+    def __init__(self, start: datetime, delta: timedelta, expired: int, func: callable, **kwargs):
         self.task_func = func
-        self.last_run_datetime = None                       # datetime of the last run
-        self.next_run_datetime = None                       # datetime of the next run
-        self.timedelta = timedelta                          # timedelta between runs
-        self.start_datetime = start_datetime                # specific datetime to start on
-        self.expire_datetime = start_datetime + (timedelta * expire_run_times)   # specific datetime to expired
-        self.run_times = 0                                  # run times
-        self.tasks = tasks                                  # scheduler to register with
+        self.last_run_datetime = datetime.now()        # datetime of the last run
+        self.next_run_datetime = start + delta                  # datetime of the next run
+        self.timedelta = delta                                  # timedelta between runs
+        self.start_datetime = start                             # specific datetime to start on
+        self.expire_datetime = start + (delta * (expired + 1))  # specific datetime to expired
+        self.run_times = 0                                      # run times
+        self.kwargs = kwargs
 
     def run(self):
         if self.task_func is not None:
-            self.task_func()
-        self.last_run_datetime = datetime.datetime.now()
+            self.task_func(**self.kwargs)
+        self.last_run_datetime = datetime.now()
         self.schedule_next_run()
 
+    def next_should_run(self):
+        return self.next_run_datetime < self.expire_datetime
+
     def should_run(self):
-        current_datetime = datetime.datetime.now()
-        is_expired = current_datetime < self.expire_datetime
-        is_run = current_datetime >= self.next_run_datetime
-        return is_expired and is_run
+        current_datetime = datetime.now()
+        is_not_expired = current_datetime < self.expire_datetime
+        is_not_run = current_datetime >= self.next_run_datetime
+        return is_not_expired and is_not_run
 
     def schedule_next_run(self):
-        self.next_run_datetime = self.start_datetime + (self.timedelta * (self.run_times + 1))
-        if self.next_run_datetime < datetime.datetime.now():
+        self.run_times += 1
+        self.next_run_datetime = self.start_datetime + (self.timedelta * self.run_times)
+        if self.next_run_datetime < datetime.now():
             self.schedule_next_run()
